61db0db93507538905767d4947e41d96a0e2aa50
Rafael de Oliveira Pereira <rafadrummer1@gmail.com>
2026-05-12 11:14:27 -0300

    sétimo exercício

---
+++ JavaScript/atividade7.js
@@ -0,0 +1,6 @@
+let idadeNecessaria = prompt("Quantos anos você tem ?")
+if(idadeNecessaria >= 18){
+    alert("Você pode jogar ! Bem-vindo !!")
+}else{
+    alert("Você não tem idade o suficiente ")
+}
