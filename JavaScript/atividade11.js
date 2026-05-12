7484ab440cda1a91462182d8d655d6300e0067f1
Rafael de Oliveira Pereira <rafadrummer1@gmail.com>
2026-05-12 11:52:08 -0300

    11º exercício

---
+++ JavaScript/atividade11.js
@@ -0,0 +1,12 @@
+let cor = "verde";
+switch(cor){
+    case "verde":
+        alert("Siga!");
+        break
+    case "amarelo":
+        alert("Atenção!");
+        break
+    case "vermelho" :
+        alert("Pare!");
+        break
+}
