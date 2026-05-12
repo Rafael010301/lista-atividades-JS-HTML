e78e667ffac1600c4c73df1180b3d5b3db3a7b67
Rafael de Oliveira Pereira <rafadrummer1@gmail.com>
2026-05-12 12:00:14 -0300

    12° exercício

---
+++ JavaScript/atividade12.js
@@ -0,0 +1,12 @@
+let numeroPedido = 1;
+switch(numeroPedido){
+    case 1:
+        alert("Você escolheu pizza! Confira nosso cardápio.");
+        break;
+    case 2:
+        alert("Você escolheu hambúrguer! Delícia!");
+        break;
+    case 3:
+        alert("Você escolheu sushi! Que tal um combinado?");
+        break;
+}
